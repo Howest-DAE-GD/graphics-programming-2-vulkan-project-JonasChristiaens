3ccb50d54de4fa4fd4687cfb49450463b5f327e3
Jonas <christiaensjonas3@gmail.com>
2025-03-18 19:35:25 +0100

    Improved bat file to work where VulkanSDK can be installed at any place in your device
    + Finshed Vertex Buffers chapter

--- shaders/compile.bat
+++ shaders/compile.bat
@@ -1,3 +1,22 @@
-C:/VulkanSDK/1.3.296.0/Bin/glslc.exe shader.vert -o vert.spv
-C:/VulkanSDK/1.3.296.0/Bin/glslc.exe shader.frag -o frag.spv
+@echo off
+setlocal
+
+:: Check if VULKAN_SDK environment variable is set
+if "%VULKAN_SDK%"=="" (
+    echo [ERROR] VULKAN_SDK environment variable is not set!
+    echo Please set it to the Vulkan SDK installation path.
+    pause
+    exit /b 1
+)
+
+:: Define GLSLC path
+set GLSLC="%VULKAN_SDK%\Bin\glslc.exe"
+
+:: Get the path to the script's directory
+set SCRIPT_DIR=%~dp0
+
+:: Compile shaders
+%GLSLC% "%SCRIPT_DIR%shader.vert" -o "%SCRIPT_DIR%vert.spv"
+%GLSLC% "%SCRIPT_DIR%shader.frag" -o "%SCRIPT_DIR%frag.spv"
+
 pause
